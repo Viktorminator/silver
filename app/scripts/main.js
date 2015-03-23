8c23a30d2519c044fb1ab02becb20b5ec77cf446
Viktorminator <viktorminator@gmail.com>
2015-03-23 03:00:32 +0200

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+$(window).ready(function () {
+   revapi = jQuery('.tp-banner').revolution(
+			{
+						delay:15000,
+						startwidth:1164,
+						startheight:517,
+						hideThumbs:10,
+						fullWidth:"off",
+						fullScreen:"off",
+						fullScreenOffsetContainer: ""
+			});
